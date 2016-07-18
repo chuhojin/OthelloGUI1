9dbe866bd404a7b20679cb6ec65c944240107dd7
chuhojin <chuhojin2@gmail.com>
2016-07-18 16:10:49 +0900

    board 25line

---
+++ src/Board.java
@@ -0,0 +1,74 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Board extends JPanel implements ActionListener{
+	Board board;
+	GameImage[][] gi;
+	JButton[][] bu=new JButton[5][5];
+	
+	public Board(Board b){
+		super();
+		setLayout(new GridLayout(0, Main.getN()));
+		board = b;
+		gi = new GameImage[Main.getN()][Main.getN()];
+		for(int i = 0; i< Main.getN(); i++){
+			for(int j = 0; j < Main.getN(); j++){
+				bu[i][j]=new JButton(new ImageIcon("image/black.jpg"));
+				//gi[i][j] = new GameImage(i,j);
+				//gi[i][j].addActionListener(this);
+				//add(gi[i][j]);
+				add(bu[i][j]);
+			}
+		}
+		
+/*		gi[3][4].setImage(1);
+		gi[4][3].setImage(1);
+		gi[3][3].setImage(2);
+		gi[4][4].setImage(2);*/
+				
+		printDosMap();
+		
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		GameImage temp = (GameImage) e.getSource();
+		Main.x = temp.getX();
+		Main.y = temp.getY();
+		/*
+		if(temp.getImage() == 1)
+			temp.setImage(2);
+		else if(temp.getImage() == 2 || temp.getImage() == 0)
+			temp.setImage(1);
+		*/
+		//printDosMap();
+		
+	}
+	
+	public void printDosMap(){
+		for(int i = 0; i< Main.getN(); i++){
+			for(int j = 0; j < Main.getN(); j++){
+				//System.out.print(gi[i][j].getImage() + " ");
+			}
+			System.out.println();
+		}
+	}
+	@Override
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+	}
+	
+	
+	
+}
